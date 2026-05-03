f136cce263d485799ac9442d54a863d250ffef37
Watchara Buasrirattana <watchara.buasrirattana@gmail.com>
2026-05-03 11:37:53 +0700

    config update

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,14 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin()
 
 const nextConfig: NextConfig = {
-
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'upload.teachingjobthailand.com',
+            },
+        ],
+    },
 }
 
 export default withNextIntl(nextConfig)
